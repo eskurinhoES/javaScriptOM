db2f4e414b937a182168e948f9d2dca7d7fea532
EskurinhoES <eskurinhoes082023@gmail.com>
2024-05-27 21:54:47 -0300

    criado e resolvidos exercicios sobre variáveis

---
+++ aulas/aula05/index.js
@@ -0,0 +1,3 @@
+console.log('Meus primeiros scripts JS')
+document.write('MINHA PRIMEIRA PÁGINA HTML COM JS')
+alert('Olá!!!')
---
+++ aulas/exercicios/exercicio001.js
@@ -0,0 +1,3 @@
+/* Escreva um script que imprima no console:
+"Olá! Meu nome é: 'NOME' e estou aprendendo Javascript.*/
+console.log('"Olá,meu nome é: "Verlã" e estou aprendendo Javascript"')
