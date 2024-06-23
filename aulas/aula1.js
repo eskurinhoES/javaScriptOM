6808f587138d713975d8b26731aeb077dfb5207e
EskurinhoES <eskurinhoes082023@gmail.com>
2024-06-23 20:46:15 -0300

    trabalhando com data

--- aulas/aula1.js
+++ aulas/aula1.js
@@ -1,1 +1,7 @@
 console.log('Hello, Word!!');
+const tresHoras = 60 * 60 * 3 * 1000;
+const data = new Date(0 + tresHoras);
+console.log(data.toString());
+
+const dataNow = new Date();
+console.log(dataNow);
