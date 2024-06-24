c871ae3138bf3d3ba81f6b034547e7c756d7a579
EskurinhoES <eskurinhoes082023@gmail.com>
2024-06-24 19:14:08 -0300

    Resolvido exercicios de data e for

---
+++ aulas/aula031/for.js
@@ -0,0 +1,9 @@
+// console.log('Linha 0');
+// console.log('Limha 1');
+// console.log('Linha 2');
+// console.log('Linha 3');
+// console.log('Linha 4');
+// console.log('Linha 5');
+for (let i = 0; i <= 500; i+=2) {
+    console.log(`Linha ${i}`);
+}
